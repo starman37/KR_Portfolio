f1ab1ed84712fbcf202feb20515b284f84b79fef
Omar <omarabdullah181628@gmail.com>
2026-01-05 15:12:09 +0600

    Update base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Portfolio/",
+  base: "/KR_Portfolio/",
   plugins: [react(), tailwindcss()],
 });
